b19cf12862c5eaee708e2dff1fbe8aa6263a10ec
MPR0 <meizumxpro0@gmail.com>
2025-07-28 18:13:47 +0300

    fix number

---
+++ build/precache-manifest.1bc1e43d479d68988430f469145856bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca2534e5064441155ec793c3336e8df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b2aa1e3859ae67f981",
+    "url": "/static/css/main.a952479e.chunk.css"
+  },
+  {
+    "revision": "5d863e465467c7869c32",
+    "url": "/static/js/2.c3628f3d.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "00b2aa1e3859ae67f981",
+    "url": "/static/js/main.c08c3ad3.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  },
+  {
+    "revision": "5f3c1d7165d225bd98ea5f00483c1dfc",
+    "url": "/static/media/ai-hero-image.5f3c1d71.svg"
+  },
+  {
+    "revision": "43b1cef43ea88bf8b1f4d7042fb69922",
+    "url": "/static/media/artem.43b1cef4.jpg"
+  },
+  {
+    "revision": "82acbfe14a1874e96ff07e89fd014fc5",
+    "url": "/static/media/dmitriy.82acbfe1.jpg"
+  },
+  {
+    "revision": "841f5db278a066e6008e2a3e90e5ba91",
+    "url": "/static/media/gregory.841f5db2.jpg"
+  },
+  {
+    "revision": "68f6654b4534da61f4ea3c81b2e95998",
+    "url": "/static/media/ivan.68f6654b.jpg"
+  },
+  {
+    "revision": "7d79411daa67252474280e6964928c93",
+    "url": "/static/media/logo.7d79411d.svg"
+  }
+]);
